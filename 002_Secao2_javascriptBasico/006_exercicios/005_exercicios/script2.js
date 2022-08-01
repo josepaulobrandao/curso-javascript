1e2b662815d1087de82644d902fa46a0c0c5a313
Jose Paulo <jose.brandao@b2wdigital.com>
2022-08-01 16:12:59 -0300

    add exercice 005

--- 002_Secao2_javascriptBasico/006_exercicios/005_exercicios/script2.js
+++
@@ -1,5 +0,0 @@
-let bodys = document.getElementById('body');
-
-let addNovoElemento = document.createElement('p');
-
-bodys.appendChild(addNovoElemento);
---
+++ 002_Secao2_javascriptBasico/006_exercicios/005_exercicios/sript2.js
@@ -0,0 +1,1 @@
+let p = document.createElement('p')
